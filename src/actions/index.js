8f319ccb4a2b6a4d2c0ad7da809a78784fe487a4
haromrozsa <haromrozsa@gmail.com>
2017-10-10 20:21:44 +0200

    Cleanup #1

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 export const CHART_RENDERED = 'CHART_RENDERED';
 export const WEEKLY_CHART_RENDERED = 'WEEKLY_CHART_RENDERED';
 
+export { addStochasticValues, fetchPrediction, deletePrediction,
+  ADD_STOCHASTIC_VALUES, FETCH_PREDICTION, DELETE_PREDICTION } from './predictionAction';
 export { clickOnSymbol, save, fetchSymbols, fetchWeeklySymbols,
   CLICK_ON_SYMBOL, SAVE_SYMBOLS, FETCH_SYMBOL, FETCH_WEEKLY_SYMBOL } from './stockAction';
 
---
+++ src/actions/predictionAction.js
@@ -0,0 +1,39 @@
+import axios from 'axios';
+export const ADD_STOCHASTIC_VALUES = 'ADD_STOCHASTIC_VALUES';
+export const FETCH_PREDICTION = 'FETCH_PREDICTION';
+export const DELETE_PREDICTION = 'DELETE_PREDICTION';
+
+//const ROOT_URL = 'http://localhost:3090';
+const ROOT_URL = 'https://seccer-api.herokuapp.com';
+
+export function addStochasticValues(values, timeFrame) {
+  //alert('TEST');
+  return {
+    type: { type: ADD_STOCHASTIC_VALUES, timeFrame: timeFrame },
+    payload: values
+  };
+}
+
+export function fetchPrediction(symbol) {
+
+  //console.log('fetchPrediction called');
+
+  const request = axios.get(`${ROOT_URL}/prediction/${symbol}` );
+
+  return {
+    type: { type: FETCH_PREDICTION },
+    payload: request
+  };
+}
+
+export function deletePrediction(symbol) {
+
+  //console.log('deletePrediction called');
+
+  const request = axios.delete(`${ROOT_URL}/prediction/${symbol}` );
+
+  return {
+    type: { type: DELETE_PREDICTION },
+    payload: request
+  };
+}
